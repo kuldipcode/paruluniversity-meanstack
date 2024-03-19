38bd0d0ed5ac0402b4d3b30fc48a74d35bfbcaa5
kuldipcode <kuldip.code@gmail.com>
2024-03-19 09:02:28 +0530

    Reactive form

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
-export class FormsComponent {
-  onClickSubmit(result:any) {
+export class FormsComponent implements OnInit {
+   userName:any; 
+   formdata:any;
+   ngOnInit() { 
+      this.formdata = new FormGroup({ 
+         userName: new FormControl("ReactiveTemplate")
+      }); 
+   } 
+   onClickSubmit(data:any) {this.userName = data.userName;}
+
+  onTemplateClickSubmit(result:any) {
     console.log("You have entered : " + result.username); 
  }
 }
