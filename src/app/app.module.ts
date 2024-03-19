6f6985daf419353b3dd802f031ea342fe28eab81
kuldipcode <kuldip.code@gmail.com>
2024-03-19 08:48:59 +0530

    add ReactiveFormsModule in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstTaskComponent } from './first-task/first-task.component';
@@ -16,6 +16,7 @@ import { FormsComponent } from './forms/forms.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
